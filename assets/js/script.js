1cd6ef20997f234dec7a3336aaa29d241b740cff
PichunLEE <pc.lee.simplon@gmail.com>
2022-03-05 18:19:03 +0100

    updated scroll but not success

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,5 @@ $(function(){
     $("#navbar").load("navbar.html");
     $("#footer").load("footer.html");
 });
+
+
---
+++ assets/js/scroll.js
@@ -0,0 +1,46 @@
+console.log('scroll');
+//Je séléctionne et je stock la DIV modal-container
+const modal =document.querySelector('.modal-container');
+console.log(modal);
+
+//Je sélectionne le bouton
+const btn=document.querySelector('.btn-success');
+console.log(btn);
+
+//Je sélectionne le code
+const code=document.querySelector('.code');
+console.log(code);
+
+//Je sélectionne l'icone
+const icone=document.querySelector('.fa-times')
+console.log(icone);
+
+window.addEventListener('scroll', displayModal);
+
+function displayModal(){
+    console.log('défilement détecté!');
+
+    let hauteur = document.documentElement.scrollTop;
+    console.log(hauteur, 'nombre de pixels depuis le haut de la page');
+
+    if (hauteur > 400){
+        console.log('affichage de la modal!');
+        modal.getElementsByClassName.display = "flex";
+    };
+}
+
+btn.addEventListener('click', function(){
+    console.log('bouton cliqué');
+    //Je rends visible le code
+    code.style.display = "block";
+    //Je rends invisible le bouton
+    btn.style.display="none";
+});
+
+icone.addEventListener('click', function(){
+    console.log('icone cliquée');
+    //Je rends invisible la DIV modal-container
+    modal.style.display="none";
+    //J'annule l'événement
+    window.removeEventListener('scroll', displayModal)
+});
